c67997877d84483d6854f0fbb773f56b440daf40
YGrylls <910357107@qq.com>
2019-04-12 21:51:51 +0800

    empty project

---
+++ src/main/java/com/petio/petIO/PetIoApplication.java
@@ -0,0 +1,13 @@
+package com.petio.petIO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetIoApplication.class, args);
+	}
+
+}
