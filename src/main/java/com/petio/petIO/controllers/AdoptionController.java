50e572041566499252e052505228275ff5166cc4
biubiubiupiu <yangguangda555@163.com>
2019-04-22 22:42:53 +0800

    add adoption detail and apply
    add adoption detail and apply except those modules related to session

--- src/main/java/com/petio/petIO/controllers/AdoptionController.java
+++ src/main/java/com/petio/petIO/controllers/AdoptionController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.petio.petIO.Utils.GeneralUtils;
 import com.petio.petIO.Utils.ResultFactory;
 import com.petio.petIO.beans.Adoption;
 import com.petio.petIO.beans.Result;
@@ -22,6 +23,8 @@ public class AdoptionController {
 	
 	@Autowired
 	private AdoptionListService adoptionListService;
+	@Autowired
+	private GeneralUtils utils;
 	
 	@CrossOrigin
 	@RequestMapping(value = "/api/adoption", method = RequestMethod.POST)
@@ -36,5 +39,39 @@ public class AdoptionController {
 																	searchInfoVo.getPage());
 		System.out.println(adoptionList);
 		return ResultFactory.buildSuccessResult(adoptionList);
+	}
+	
+	@CrossOrigin
+	@RequestMapping(value = "/api/adoption/detail/{id}", method = RequestMethod.POST)
+	@ResponseBody
+	public Result getDetail(@RequestBody Integer id, BindingResult bindingResult) {
+		Adoption adoption = adoptionListService.getAdoptionByID(id);
+		if(adoption == null)return ResultFactory.buildFailResult("未找到用户");
+		
+		adoption.setImgPaths(utils.getImgPaths(id));   //获取图片路径，暂时没实现
+		
+		
+		
+		return ResultFactory.buildSuccessResult(adoption);
+	}
+	
+	@CrossOrigin
+	@RequestMapping(value = "/api/adoption/apply", method = RequestMethod.POST)
+	@ResponseBody
+	public Result Apply(@RequestBody Integer id,
+			BindingResult bindingResult) {
+		
+		int uid = 1;   //通过session获取用户id
+		int times = adoptionListService.getApplyTimes(uid);  //获取用户当前申请次数 
+		if(times > 3)return ResultFactory.buildFailResult("超过今日申请次数！");
+		
+		if(!adoptionListService.checkApply(id, uid)) {    //没申请过
+			adoptionListService.addOffer(id, uid);
+			adoptionListService.addApplyTimes(id, uid);     //增加今日申请次数，暂时没实现
+			return ResultFactory.buildSuccessResult("申请成功！");
+		}
+		else {
+			return ResultFactory.buildSuccessResult("您已申请过！");
+		}
 	}
 }
--- src/main/java/com/petio/petIO/mapper/AdoptionListMapper.java
+++ src/main/java/com/petio/petIO/mapper/AdoptionListMapper.java
@@ -2,7 +2,9 @@ package com.petio.petIO.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.SelectProvider;
 
 import com.petio.petIO.Utils.SqlProvider;
@@ -16,4 +18,18 @@ public interface AdoptionListMapper {
 	
 	@SelectProvider(type=SqlProvider.class,method="getAdoption")
 	public List<Adoption> getFosterage(String title,String location,String type,Integer page);
+	
+	@Select("select * from Adoption where aID = #{id}")
+	public Adoption getAdoptionByID(Integer id);
+	
+	@Select("select times from ApplyTimes where id = #{id}")
+	public Integer getApplyTimes(Integer id);
+	
+	@Select("select count(*) from Apply where aID = #{aID} and applier = #{uID}")
+	public Integer getApply(Integer aID, Integer uID);
+	
+	@Insert("insert into Apply (aID,applier)values(#{aID},#{uID})")
+	public Integer addOffer(Integer aID,Integer uID);
+
+	
 }
