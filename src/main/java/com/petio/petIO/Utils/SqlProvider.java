90b6d33132d29286a7f6e043bd6f9e11b34ffe12
YGrylls <910357107@qq.com>
2019-04-24 16:22:59 +0800

    fix chinese query problem

--- src/main/java/com/petio/petIO/Utils/SqlProvider.java
+++ src/main/java/com/petio/petIO/Utils/SqlProvider.java
@@ -2,7 +2,8 @@ package com.petio.petIO.Utils;
 
 public class SqlProvider {
 	private int noteNumber = 20;
-	public String getAdoptionTotalNumber(String title,String location,String type){
+
+	public String getAdoptionTotalNumber(String title, String location, String type) {
 		StringBuffer sql = new StringBuffer("select count(*) from Adoption where aState = 1");
 		if (!"All".equals(location)) {
 			sql.append(" and location=#{location}");
@@ -10,14 +11,15 @@ public class SqlProvider {
 		if (!"All".equals(type)) {
 			sql.append(" and aType=#{type}");
 		}
-		
-		if (title!=null&&!"".equals(title)) {
-			System.out.println("[title:"+title+"]");
-			sql.append(" and aTitle LIKE '%"+title+"%'");
+
+		if (title != null && !"".equals(title)) {
+			System.out.println("[title:" + title + "]");
+			sql.append(" and aTitle LIKE '%" + title + "%'");
 		}
 		return sql.toString();
 	}
-	public String getAdoption(String title,String location,String type,Integer page){
+
+	public String getAdoption(String title, String location, String type, Integer page) {
 		StringBuffer sql = new StringBuffer("select * from Adoption where aState = 1");
 		if (!"All".equals(location)) {
 			sql.append(" and location=#{location}");
@@ -25,16 +27,17 @@ public class SqlProvider {
 		if (!"All".equals(type)) {
 			sql.append(" and aType=#{type}");
 		}
-		
-		if (title!=null&&!"".equals(title)) {
-			System.out.println("[title:"+title+"]");
-			sql.append(" and aTitle LIKE '%"+title+"%'");
+
+		if (title != null && !"".equals(title)) {
+			System.out.println("[title:" + title + "]");
+			sql.append(" and aTitle LIKE '%" + title + "%'");
 		}
-		if (page<1) {
+		if (page < 1) {
 			page = 1;
 		}
-		int offset = 20*(page-1);
-		sql.append(" limit "+offset+" , "+noteNumber);
+		int offset = 20 * (page - 1);
+		sql.append(" limit " + offset + " , " + noteNumber);
+		System.out.println(sql.toString());
 		return sql.toString();
 	}
 }
--- src/main/java/com/petio/petIO/mapper/AdoptionListMapper.java
+++ src/main/java/com/petio/petIO/mapper/AdoptionListMapper.java
@@ -2,25 +2,20 @@ package com.petio.petIO.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.SelectProvider;
-import org.apache.ibatis.annotations.Update;
 
 import com.petio.petIO.Utils.SqlProvider;
 import com.petio.petIO.beans.Adoption;
 
-
 @Mapper
 public interface AdoptionListMapper {
-	@SelectProvider(type=SqlProvider.class,method="getAdoptionTotalNumber")
-	public Integer getTotalNumber(String title,String location,String type);
-	
-	@SelectProvider(type=SqlProvider.class,method="getAdoption")
+	@SelectProvider(type = SqlProvider.class, method = "getAdoptionTotalNumber")
+	public Integer getTotalNumber(String title, String location, String type);
 
-	public List<Adoption> getAdoption(String title,String location,String type,Integer page);
+	@SelectProvider(type = SqlProvider.class, method = "getAdoption")
+	public List<Adoption> getAdoption(String title, String location, String type, Integer page);
 
-	public List<Adoption> getFosterage(String title,String location,String type,Integer page);
-	
+	public List<Adoption> getFosterage(String title, String location, String type, Integer page);
+
 }
