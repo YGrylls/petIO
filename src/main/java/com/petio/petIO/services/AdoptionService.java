274ccc752c63c9bc4e1e851d8661d26e3f43cac1
biubiubiupiu <yangguangda555@163.com>
2019-05-06 17:12:24 +0800

    get published adoptions

--- src/main/java/com/petio/petIO/services/AdoptionService.java
+++ src/main/java/com/petio/petIO/services/AdoptionService.java
@@ -1,5 +1,7 @@
 package com.petio.petIO.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -42,5 +44,9 @@ public class AdoptionService {
 	
 	public void initApplyTimes() {
 		mapper.initApplyTimes();
+	}
+	
+	public List<Adoption> getAdoptionsByUid(Integer uid){
+		return mapper.getAdoptionsByUid(uid);
 	}
 }
