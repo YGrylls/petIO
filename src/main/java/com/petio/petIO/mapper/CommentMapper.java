1b9dc30dcee2543da6e3c8ee6b272124932750e0
zw6666 <2714709357@qq.com>
2019-05-29 16:38:01 +0800

    change unread

--- src/main/java/com/petio/petIO/mapper/CommentMapper.java
+++ src/main/java/com/petio/petIO/mapper/CommentMapper.java
@@ -16,7 +16,7 @@ public interface CommentMapper {
 	@Select("select Comment.cID, Comment.commentText, u1.username as fromUser, u2.userName as toUser, Comment.pubtime from Comment inner join User u1 on Comment.from = u1.userID and Comment.aID = #{aID} left join User u2 on Comment.to = u2.userID order by Comment.pubtime")
 	public List<CommentInfo> getCommentsByAdoptionID(Integer aID);
 	
-	@Select("select Comment.aID, aTitle, username , commentText as content , pubtime as time from User , Comment , Adoption where Comment.to = #{userID} and User.userID = Comment.from and Adoption.aID = Comment.aID")
+	@Select("select Comment.aID, aTitle, username , commentText as content , pubtime as time from User , Comment , Adoption where Comment.to = #{userID} and User.userID = Comment.from and Adoption.aID = Comment.aID and Comment.read=0")
 	public List<NewInfo> getUnreadCommentsByuserID(Integer userID);
 	
 	@Select("select count(*) from User , Comment , Adoption where Comment.to = #{userID} and User.userID = Comment.from and Adoption.aID = Comment.aID")
